0e3a2744f20bc7153ac5bc7da37585d757eb7627
shuchitasingh <singhshuchita5@gmail.com>
2022-02-21 11:25:04 +0530

    ADDED CODE

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        I am learning React. My life is getting better.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Hello Learn Reactjs
+        </a>
+      </header>
+    </div>  
+  );
+}
+
+export default App;
